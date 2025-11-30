fdf80c26e7604f9e47426c28f3333c29c828cc3d
karenelizabg <karene.gonzalez@iteso.com>
2025-11-29 19:36:07 -0600

    mover archivos a data para Dgraph

--- Dgraph/model.py
+++ Dgraph/model.py
@@ -58,23 +58,23 @@ def set_schema(client):
 
 def create_data(client):
 #primero cargamos los noods
-    marcas_uids = load_marcas(client, 'Dgraph/data/marcas.csv')
-    categorias_uids = load_categorias(client, 'Dgraph/data/categorias.csv')
-    productos_uids = load_productos(client, 'Dgraph/data/productos.csv')
-    usuarios_uids = load_usuarios(client, 'Dgraph/data/usuarios.csv')
+    marcas_uids = load_marcas(client, 'data/Dgraph/marcas.csv')
+    categorias_uids = load_categorias(client, 'data/Dgraph/categorias.csv')
+    productos_uids = load_productos(client, 'data/Dgraph/productos.csv')
+    usuarios_uids = load_usuarios(client, 'data/Dgraph/usuarios.csv')
 
 #despues cargar relaciones
-    load_relaciones(client, "Dgraph/data/relacion_pertenece.csv", productos_uids, marcas_uids, "PERTENECE_A")
-    load_relaciones(client, "Dgraph/data/relacion_en_categoria.csv", productos_uids, categorias_uids, "EN_CATEGORIA")
-    load_relaciones(client, "Dgraph/data/relacion_compro.csv", usuarios_uids, productos_uids, "COMPRO")
-    load_relaciones(client, "Dgraph/data/relacion_vio.csv", usuarios_uids, productos_uids, "VIO")
-    load_relaciones(client, "Dgraph/data/relacion_favoritos.csv", usuarios_uids, productos_uids, "AGREGO_A_FAVORITOS")
+    load_relaciones(client, "data/Dgraph/relacion_pertenece.csv", productos_uids, marcas_uids, "PERTENECE_A")
+    load_relaciones(client, "data/Dgraph/relacion_en_categoria.csv", productos_uids, categorias_uids, "EN_CATEGORIA")
+    load_relaciones(client, "data/Dgraph/relacion_compro.csv", usuarios_uids, productos_uids, "COMPRO")
+    load_relaciones(client, "data/Dgraph/relacion_vio.csv", usuarios_uids, productos_uids, "VIO")
+    load_relaciones(client, "data/Dgraph/relacion_favoritos.csv", usuarios_uids, productos_uids, "AGREGO_A_FAVORITOS")
 
     #generar diccionarios para simplificar el menu
-    marcas_dicc = csv_a_diccionario('Dgraph/data/marcas.csv')
-    categoria_dicc = csv_a_diccionario('Dgraph/data/categorias.csv')
-    productos_dicc = csv_a_diccionario('Dgraph/data/productos.csv')
-    usuarios_dicc = csv_a_diccionario('Dgraph/data/usuarios.csv')
+    marcas_dicc = csv_a_diccionario('data/Dgraph/marcas.csv')
+    categoria_dicc = csv_a_diccionario('data/Dgraph/categorias.csv')
+    productos_dicc = csv_a_diccionario('data/Dgraph/productos.csv')
+    usuarios_dicc = csv_a_diccionario('data/Dgraph/usuarios.csv')
 
      #retornar los diccionarios
     return {
